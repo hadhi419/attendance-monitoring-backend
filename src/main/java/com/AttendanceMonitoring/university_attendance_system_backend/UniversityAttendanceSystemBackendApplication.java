ccffa7e94fbafc68a430213a68e2a6eaa1ef90ce
hadhi419 <dev.hadhi419@gmail.com>
2025-08-09 00:03:25 +0530

    fixed a bug

--- src/main/java/com/AttendanceMonitoring/university_attendance_system_backend/UniversityAttendanceSystemBackendApplication.java
+++ src/main/java/com/AttendanceMonitoring/university_attendance_system_backend/UniversityAttendanceSystemBackendApplication.java
@@ -2,13 +2,20 @@ package com.AttendanceMonitoring.university_attendance_system_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class UniversityAttendanceSystemBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UniversityAttendanceSystemBackendApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(UniversityAttendanceSystemBackendApplication.class, args);
+
+		Environment env = context.getEnvironment();
+		String address = env.getProperty("server.address", "0.0.0.0");
+		String port = env.getProperty("server.port", "8080");
+
+		System.out.println("Server configured to listen on: " + address + ":" + port);
 	}
 
 }
-
