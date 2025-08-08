7dccf548f37b86a6b0738b5867bad348c1de26be
hadhi419 <dev.hadhi419@gmail.com>
2025-08-09 01:26:26 +0530

    fixed a bug

--- src/test/java/com/AttendanceMonitoring/university_attendance_system_backend/UniversityAttendanceSystemBackendApplicationTests.java
+++ src/test/java/com/AttendanceMonitoring/university_attendance_system_backend/UniversityAttendanceSystemBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.AttendanceMonitoring.university_attendance_system_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UniversityAttendanceSystemBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
